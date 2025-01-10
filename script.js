64e366bd3e1ff8cb52d1ab3d1381120d760b61a9
Alicia <aliciakfinacio@gmail.com>
2025-01-10 20:05:31 -0300

    finalização da responsividade no CSS

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const resumeBtns = document.querySelectorAll('.resume-btn');
 const navLinks = document.querySelectorAll('header nav a');
 const logoLink = document.querySelector('.logo');
 const sections = document.querySelectorAll('section');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('header nav');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+});
 
 const activePage = () => {
     const header = document.querySelector('header');
@@ -24,6 +31,9 @@ const activePage = () => {
     sections.forEach(section => {
         section.classList.remove('active');
     });
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
 
 navLinks.forEach((link,idx) => {
